bee39a36fb661a58f1cbd9644ebefb2125734818
LiliaSkaletska <32746093+LiliaSkaletska@users.noreply.github.com>
2020-05-29 16:27:59 +0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,10 @@
+insert into tour(tour_name,country,year_category,tour_duration,price_range,tour_price) values ('Paris dream','France','winter tour','7 days','500-1000 dollars','550');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Venecia weekend','Italy','summer tour','10 days','500-1000 dollars','700');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Lunch in Bratislava','Slovakia','spring tour','3 days','100-500 dollars','150');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Date in Rome','Italy','autumn tour','7 days','500-1000 dollars','650');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Sea fairytale','France','summer tour','5 days','500-1000 dollars','750');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Western traditions','USA','spring tour','10 days','1000+ dollars','1550');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Meet Budapesht','Hungary','summer tour','3 days','500-1000 dollars','250');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Football in Barcelona','Spain','winter tour','7 days','500-1000 dollars','450');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Disneyland','France','winter tour','5 days','500-1000 dollars','350');
+insert into tour(tour_name,country,year_category,duration_tour,price_range,tour_price) values ('Viena fairytale','Austria','spring tour','3 days','500-1000 dollars','250');
